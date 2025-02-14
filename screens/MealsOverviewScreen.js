fca0505cec0084455caca85f6e37eecee4255c65
kashtakov <kashtakovs@gmail.com>
2025-02-14 17:22:28 +0200

    14-02-25

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,14 +1,31 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
+
+
 
 
 function MealsOverviewScreen ({route}){
 
    const catId = route.params.categoryId;
 
+   const dispayedMeals = MEALS.filter((mealItem)=>{
+        return mealItem.categoryIds.indexOf(catId) >= 0;
+   });
+
+   function renderMealItem(itemData){
+         return(
+             <MealItem title={itemData.item.title}/>
+         )
+   }
+
     return(
         <View style={styles.container}>
-            <Text>Meals Overview Screen - {catId}</Text>
+            <FlatList 
+            data={dispayedMeals}
+            keyExtractor={(item)=>item.id}
+            renderItem={({item})=> <Text>{item.title}</Text>}
+            />
         </View>
     )
 }
